1c5f35a9ce9d77bb80828d28021c5ebe6b5bbfb5
2betop <liaoxuezhi@icloud.com>
2015-03-27 10:54:28 +0800

    commit

--- sample/fis-conf.js
+++ sample/fis-conf.js
@@ -1,3 +1,11 @@
-fis.match('(*.js)', {
+fis.match('(*.html)', {
+    release: '/html/$1'
+});
+
+fis.media('dev').match('(*.js)', {
     release: '/static/$1'
+});
+
+fis.media('production').match('(*.js)', {
+    release: '/production/$1'
 });
