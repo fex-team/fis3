be5cc38aaf689a861157b3cdcb096ece0f14c31e
xiangshouding <fansekey@gmail.com>
2015-04-23 18:51:22 +0800

    some change or add test

---
+++ test/file/fis-config-test/index.js
@@ -0,0 +1,11 @@
+    /*
+     * fis
+     * http://fis.baidu.com/
+     */
+
+    'use strict';
+
+    module.exports = function(content){
+        console.log(content)
+        return content + '--configTEST--';
+    };
