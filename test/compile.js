50b45cc4b856cdc4732579d794f107d8d44c839a
xiangshouding <fansekey@gmail.com>
2015-04-28 17:36:24 +0800

    compile.js test.ut

---
+++ test/compile.js
@@ -0,0 +1,79 @@
+var expect = require('chai').expect;
+var fis = require('..');
+var path = require('path');
+
+describe('compile: build single file', function () {
+  var root = path.join(__dirname, 'compile');
+  beforeEach(function () {
+    fis.project.setProjectRoot(root);
+    fis.media().init();
+    fis.config.init();
+    fis.compile.setup();
+  });
+
+  it ('build a empty .js file', function () {
+    var file = fis.file.wrap(path.join(root, 'files', 'empty.js'));
+    fis.compile(file);
+    expect(file.getContent()).to.be.equal('');
+  });
+});
+
+describe('compile: build a virtual file', function () {
+  var root = path.join(__dirname, 'compile');
+  beforeEach(function () {
+    fis.project.setProjectRoot(root);
+    fis.media().init();
+    fis.config.init();
+    fis.compile.setup();
+  });
+
+  it('compile a .js file', function () {
+    var file = fis.file.wrap('a.js');
+    file.setContent('alert("fis");');
+    fis.compile(file);
+    expect(file.getContent()).to.be.equal('alert("fis");');
+    expect(file.getHash()).to.be.equal(fis.util.md5('alert("fis");'));
+  });
+
+  it('compile a .css file', function () {
+    var file = fis.file.wrap('a.css');
+    file.setContent('body {\n color: #FCFCFC; \n }');
+    fis.compile(file);
+    expect(file.getContent()).to.be.equal('body {\n color: #FCFCFC; \n }');
+  });
+});
+
+describe('compile: builtin require', function () {
+  var root = path.join(__dirname, 'compile', 'proj', 'require');
+  beforeEach(function () {
+    fis.project.setProjectRoot(root);
+    fis.media().init();
+    fis.config.init();
+    fis.compile.setup();
+  });
+
+  //__require();
+  it ('compile .js file', function () {
+    var file = fis.file.wrap(path.join(root, 'main.js'));
+    file.useCache = false;
+    fis.compile(file);
+    expect(file.requires).to.be.deep.equal(['comp_a.js', 'comp_b.js']);
+    expect(file.getContent()).to.be.equal(fis.util.read(path.join(root, 'expect', 'main.js')));
+  });
+
+  it ('compile .css file', function () {
+    var file = fis.file.wrap(path.join(root, 'main.css'));
+    file.useCache = false;
+    fis.compile(file);
+    expect(file.requires).to.be.deep.equal(['comp_a.css']);
+    expect(file.getContent()).to.be.equal(fis.util.read(path.join(root, 'expect', 'main.css')));
+  });
+
+  it ('compile .html file', function () {
+    var file = fis.file.wrap(path.join(root, 'main.html'));
+    file.useCache = false;
+    fis.compile(file);
+    expect(file.requires).to.be.deep.equal(['comp_a.css', 'comp_a.js']);
+    expect(file.getContent()).to.be.equal(fis.util.read(path.join(root, 'expect', 'main.html')));
+  });
+});
---
+++ test/compile/proj/require/expect/main.js
@@ -0,0 +1,4 @@
+var a = require('comp_a.js');
+var a = require('comp_a.js');
+var b = require('comp_b.js');
+var b = require('comp_b.js');
