99bb7fb25f85b0a905a50829eb1d6edf342f1c8a
wangrui10 <wangrui10@baidu.com>
2015-05-14 18:45:15 +0800

    update test/fis3-plugin

--- test-fis3-plugin/module/test/index.js
+++ test-fis3-plugin/module/test/index.js
@@ -9,16 +9,38 @@ var _      = fis.util,
 var expect = require('chai').expect;
 var fis3_plugin_module = require('fis3-plugin-module');
 
-describe('index: init', function () {
-  var project;
-  beforeEach(function () {
-    fis.config.init(); // @TODO
-    fis.media().init(); // @TODO
+describe('index:', function () {
+
+  it('standard:js', function () {
+    var project;
+    _.pipe('plugin',function (processor, settings, key, type){
+      processor(fis, settings);
+      expect(type == 'plugin').to.be.true;
+
+      project = require('../../../lib/project');
+      var root = path.join(__dirname, 'project');
+      project.setProjectRoot(root);
+      settings.type = "";
+      settings.type = "amd";
+      processor(fis, settings);
+      expect(type == 'plugin').to.be.true;
+      var filepath = path.join(root, 'test2.js');
+      var file = fis.file.wrap(filepath);
+      var info = {
+        file: file,
+        content: file.getContent()
+      };
+      fis.emit('standard:js',info);
+
+    });
   });
 
   it("lookup:file", function () {
+    var project;
+
+    project = require('../../../lib/project');
     var root = path.join(__dirname, 'project');
-    fis.project.setProjectRoot(root);
+    project.setProjectRoot(root);
     var filepath = path.join(root, 'test.js');
     fis.util.write(filepath, 'console.log("hello, world.");');
     var info = project.lookup('/test.js');
@@ -36,19 +58,22 @@ describe('index: init', function () {
     expect(info.moduleId == "test.js").to.be.true;
   });
 
-  it("lookup:file", function () {
-    var root = path.join(__dirname, 'project');
-    fis.project.setProjectRoot(root);
-    var path = '/test.js';
-    var file='';
-    var info = fis.uri(path);
-    var vfile = info.file;
-    var file2 = fis.file.wrap("/test.js");
-    fis.emit('lookup:file', file2);
+  it("standard:js", function () {
+    var project;
 
-    expect(info.id == "test.js").to.be.true;
-    expect(info.moduleId == "test.js").to.be.true;
+    project = require('../../../lib/project');
+    var root = path.join(__dirname, 'project');
+    project.setProjectRoot(root);
+    var filepath = path.join(root, 'test3.js');
+
+    var file = fis.file.wrap(filepath);
+    var info = {
+      file: file,
+      content: file.getContent()
+    };
+    fis.emit('standard:js',info);
   });
+
 });
 
 describe('fis3-plugin-module compile:postprocessor', function() {
@@ -63,7 +88,7 @@ describe('fis3-plugin-module compile:postprocessor', function() {
     var content = file.getContent();
     fis.emit('compile:postprocessor', file);
     expect(file.getContent()).to.equal('define(\'' + (file.moduleId || file.id) + '\', function(require, exports, module) {\n\n' + content + '\n\n});\n');
-  });    
+  });
 
   it('compile AMD JS file', function() {
     var file = fis.file(__dirname + '/project/dmath.js');
