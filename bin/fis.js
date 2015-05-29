8a54a8a14b19e483e9af855abd1e73e7e7a7b796
xiangshouding <fansekey@gmail.com>
2015-05-29 08:27:04 +0800

    优化提示

--- bin/fis.js
+++ bin/fis.js
@@ -4,7 +4,7 @@ var Liftoff = require('liftoff');
 var argv = require('minimist')(process.argv.slice(2));
 var path = require('path');
 var cli = new Liftoff({
-  name: 'fis',
+  name: 'fis3',
   processTitle: 'fis',
   moduleName: 'fis3',
   configName: 'fis-conf',
@@ -20,12 +20,14 @@ cli.launch({
   configPath: argv.f || argv.file
 }, function(env) {
   var fis;
-
+  var location = path.dirname(env.modulePath);
   if (!env.modulePath) {
     fis = require('../');
+    location = path.join(__dirname, '../');
   } else {
     fis = require(env.modulePath);
   }
+  fis.log.info('Currently running %s (%s)', cli.name, location);
   fis.set('localNPMFolder', path.join(env.cwd, 'node_modules/fis3'));
   fis.set('globalNPMFolder', path.dirname(__dirname));
   fis.cli.run(argv, env);
