f01e5aec0892711ac2eb98bffc3b1afbe1d4ab9e
xiangshouding <fansekey@gmail.com>
2015-07-02 12:33:16 +0800

    update

--- release-doc.sh
+++ release-doc.sh
@@ -1,4 +1,5 @@
 git checkout master
+rev="fex-team/fis3@$(git log --pretty=format:'%h' -n 1)"
 cd ./doc && npm install && node ../bin/fis.js release prod -d ./output
 cd ./output && git init && git remote add origin https://github.com/fex-team/fis3.git
 
@@ -9,7 +10,7 @@ echo "https://${GH_TOKEN}:@github.com" > .git/credential
 
 git push origin :gh-pages
 git add *
-git commit -am "build fex-team/fis3@$(git log --pretty=format:'%h' -n 1)"
+git commit -am "build from ${rev}"
 
 git branch gh-pages
 git checkout gh-pages
