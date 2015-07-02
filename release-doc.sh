43809287b5ba8824a0dcfd5ec9f7d17bc911ea5a
xiangshouding <fansekey@gmail.com>
2015-07-02 12:20:39 +0800

    update

--- release-doc.sh
+++ release-doc.sh
@@ -5,8 +5,10 @@ git config --global user.email "fansekey@gmail.com"
 git config --global user.name "xiangshouding"
 git config credential.helper "store --file=.git/credential"
 echo "https://${GH_TOKEN}:@github.com" > .git/credential
+git push origin :gh-pages
 git branch gh-pages
 git checkout gh-pages
 git add *
 git commit -am "build fex-team/fis3@$(git log --pretty=format:'%h' -n 1)"
 git push -f origin gh-pages
+git checkout master
