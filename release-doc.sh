7f1c64b0da606a2827e2a5d66a928bf841bb5869
xiangshouding <fansekey@gmail.com>
2015-07-02 12:28:34 +0800

    update

--- release-doc.sh
+++ release-doc.sh
@@ -1,14 +1,20 @@
-git checkout dev
-cd doc && npm install && node ../bin/fis.js release prod -d ./output
+git checkout master
+cd ./doc && npm install && node ../bin/fis.js release prod -d ./output
 cd ./output && git init && git remote add origin https://github.com/fex-team/fis3.git
+
 git config --global user.email "fansekey@gmail.com"
 git config --global user.name "xiangshouding"
 git config credential.helper "store --file=.git/credential"
 echo "https://${GH_TOKEN}:@github.com" > .git/credential
+
 git push origin :gh-pages
-git branch gh-pages
-git checkout gh-pages
 git add *
 git commit -am "build fex-team/fis3@$(git log --pretty=format:'%h' -n 1)"
+
+git branch gh-pages
+git checkout gh-pages
+
 git push -f origin gh-pages
+cd ../../
+rm -rf ./doc/output
 git checkout master
