d7fdfd29afd735d6d81969360c5f4f2b97b2cd77
wangrui10 <wangrui10@baidu.com>
2015-09-01 14:32:01 +0800

    test

--- test/diff_fis3_smarty/inline_diff/inline_renderer.php
+++ test/diff_fis3_smarty/inline_diff/inline_renderer.php
@@ -34,7 +34,7 @@
     {
         $value="";
         foreach ($lines as $line) {
-            $value.="$line ";
+            $value.="$line <br>";
             // FIXME: don't output space if it's the last line.
         }
 
