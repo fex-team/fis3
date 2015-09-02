23f25a4661153c62a672974eb64f97e8830794d1
wangrui10 <wangrui10@baidu.com>
2015-09-02 09:56:26 +0800

    test

--- test/diff_fis3_smarty/old_fis_ready.sh
+++ test/diff_fis3_smarty/old_fis_ready.sh
@@ -3,6 +3,11 @@
 set timeout 360
 set p [lindex $argv 0]
 set o [lindex $argv 1]
+spawn su npm install fis3-smarty --registry https://registry.npm.taobao.org
+expect "Password:"
+send $p\r
+expect eof
+
 spawn su - fis -c "sh /home/work/repos/fis3.0/test/diff_fis3_smarty/release$o.sh old"
 expect "Password:"
 send $p\r
