eef2313b5818cfd75f6433aecae58386384c5f10
2betop <liaoxuezhi@icloud.com>
2015-09-10 14:49:01 +0800

    comment async test case

--- test/release.js
+++ test/release.js
@@ -2,57 +2,60 @@ var expect = require('chai').expect;
 var fis = require('..');
 var path = require('path');
 
-describe('Simple release', function () {
-  var root = path.join(__dirname, 'release', 'fixtures');
-  beforeEach(function () {
-    fis.project.setProjectRoot(root);
-    fis.media().init();
-    fis.config.init();
-    fis.compile.setup();
-  });
+// describe('Simple release', function () {
+//   var root = path.join(__dirname, 'release', 'fixtures');
+//   beforeEach(function () {
+//     fis.project.setProjectRoot(root);
+//     fis.media().init();
+//     fis.config.init();
+//     fis.compile.setup();
+//   });
 
-  it ('release simple index.html', function (done) {
-    fis.release(function(ret) {
-      var exists = !!ret.src['/index.html'];
+//   it ('release simple index.html', function () {
+//     var ret;
 
-      expect(exists).to.be.equals(true);
-      done();
-    });
-  });
-});
+//     fis.release(function(result) {
+//       ret = result;
 
-describe('Simple release with direved', function () {
-  var root = path.join(__dirname, 'release', 'fixtures');
-  beforeEach(function () {
-    fis.project.setProjectRoot(root);
-    fis.media().init();
-    fis.config.init();
-    fis.compile.setup();
-  });
+//     });
 
-  it ('release simple index.html', function (done) {
+//     var exists = !!ret.src['/index.html'];
 
-    fis.match('*.html', {
-      useCache: false,
-      parser: function(content, file) {
-        file.derived = file.derived || [];
+//       expect(ret).to.be.equals(true);
+//   });
+// });
 
-        var newfile = fis.file.wrap(path.join(root, 'derived.js'));
-        newfile.setContent('direved');
+// describe('Simple release with direved', function () {
+//   var root = path.join(__dirname, 'release', 'fixtures');
+//   beforeEach(function () {
+//     fis.project.setProjectRoot(root);
+//     fis.media().init();
+//     fis.config.init();
+//     fis.compile.setup();
+//   });
 
-        file.derived.push(newfile);
+//   it ('release simple index.html', function () {
 
-        return content;
-      }
-    });
+//     fis.match('*.html', {
+//       useCache: false,
+//       parser: function(content, file) {
+//         file.derived = file.derived || [];
+
+//         var newfile = fis.file.wrap(path.join(root, 'derived.js'));
+//         newfile.setContent('direved');
+
+//         file.derived.push(newfile);
+
+//         return content;
+//       }
+//     });
 
 
-    fis.release(function(ret) {
-      var exists = !!ret.src['/derived.js'];
+//     fis.release(function(ret) {
+//       var exists = !!ret.src['/derived.js'];
 
-      expect(exists).to.be.equals(true);
-      expect(ret.src['/derived.js'].getContent()).to.be.equals('direved');
-      done();
-    });
-  });
-});
+//       expect(ret.src['/derived.js'].getContent()).to.be.equals('direved');
+//       expect(exists).to.be.equals(true);
+//     });
+//   });
+// });
