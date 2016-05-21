bafc7b58da82764d3241e05f8962fb06921511a9
2betop <liaoxuezhi@icloud.com>
2016-05-22 00:46:27 +0800

    调整 fis.require 策略，采用路径优先，而不是名字优先

--- bin/fis.js
+++ bin/fis.js
@@ -27,11 +27,10 @@ cli.launch({
   }
 
   process.title = this.name +' ' + process.argv.slice(2).join(' ') + ' [ ' + env.cwd + ' ]';
-  
+
   // 配置插件查找路径，优先查找本地项目里面的 node_modules
   // 然后才是全局环境下面安装的 fis3 目录里面的 node_modules
-  fis.require.paths.push(path.join(env.cwd, 'node_modules/fis3'));
-  fis.require.paths.push(path.join(path.dirname(__dirname)));
+  fis.require.paths.unshift(path.join(env.cwd, 'node_modules'));
   fis.cli.name = this.name;
   fis.cli.run(argv, env);
 });
